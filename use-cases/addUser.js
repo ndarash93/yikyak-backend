e6b6ae67eeeb2a2d0c80b50696fab072c579c690
ndarash93 <nickdarash@gmail.com>
2019-11-20 20:56:34 -0700

    No change

--- use-cases/addUser.js
+++ use-cases/addUser.js
@@ -1,7 +1,6 @@
 module.exports = function makeAddUser(insert, makeUser, getUser) {
   return async function addUser(httpRequest){
     if (await getUser(httpRequest.body)){
-      console.log(httpRequest.body.phoneNumber)
       throw new Error('User with that phone number already exists!');
     }
     const user = makeUser(httpRequest.body);
