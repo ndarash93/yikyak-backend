bdc8037600dd379e5f0aa74c78d378838ba6b9c3
ndarash93 <nickdarash@gmail.com>
2019-12-10 21:41:28 -0700

    Not much

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const makeCallBack = require("./express-callback");
 const controller = require("./controllers");
 const { protectRoute } = require("./middleware");
 const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const io = require('socket.io')();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({allowHeaders: '*'}));
+
+app.options('*', cors());
 
 app.post("/add-post", protectRoute, makeCallBack(controller.post.addPost));
 app.post("/like-post", protectRoute, makeCallBack(controller.post.likePost));
@@ -20,6 +26,14 @@ app.get("/get-posts", makeCallBack(controller.post.getPosts));
 
 app.post("/add-user", makeCallBack(controller.user.addUser));
 app.post("/refresh-tokens", makeCallBack(controller.user.refreshTokens));
+
+app.get('/', cors(), (req, res) => res.json({}));
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    console.log(`Message: `);
+  });
+});
 
 app.listen(process.env.API_PORT || process.env.HOST_PORT, () => {
   console.log(
